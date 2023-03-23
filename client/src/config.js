c53a6dd125843911d33358d569f75940e73bb42a
ast_vnovodid <v.novodid@astoundcommerce.com>
2023-03-23 07:55:27 +0200

    small fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,10 @@
-const apiId = 'pxmoxb5t2j';
+// should be checked in the console after deploying serverless file!!!
+const apiId = '';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
 
+// should be taken from Auth0 web
 export const authConfig = {
-    domain: 'dev-imwin-7q.us.auth0.com', // Auth0 domain
-    clientId: 'NLyzSrs5m4vWlAFRUMWIsU7rDQsIt1yf', // Auth0 client id
+    domain: '', // Auth0 domain
+    clientId: '', // Auth0 client id
     callbackUrl: 'http://localhost:3000/callback',
 };
